e79426743d8e10bccb37b93d8d6fa2fa4f652005
Henry <hnguyhenry@protonmail.com>
2023-06-27 00:26:37 -0700

    Edit daisyui theme

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,15 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-  theme: {
-    extend: {},
-  },
+  content: ['./src/**/*.{astro,html,svelte,vue,js,ts,jsx,tsx}'],
   plugins: [require('daisyui')],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ['"Poppins"', 'sans-serif'],
+      },
+    },
+  },
+  daisyui: {
+    themes: ['retro', 'coffee'],
+  },
 };
