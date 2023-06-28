8d514b877c8f1c6a3b5b76d0f71bcfe4f087e87f
Henry <hnguyhenry@protonmail.com>
2023-06-28 01:34:28 -0700

    Add hovering animation to span elements

--- tailwind.config.js
+++ tailwind.config.js
@@ -32,15 +32,29 @@ module.exports = {
           '80%': { transform: 'scale(2) ', opacity: '1' },
           '100%': { transform: 'scale(1) ', opacity: '1' },
         },
+        webHover: {
+          '0%, 100%': { transform: 'translateY(0px) rotate(-30deg)' },
+          '50%': { transform: 'translateY(10px) rotate(-30deg)' },
+        },
+        programHover: {
+          '0%, 100%': { transform: 'translateY(0px) rotate(30deg)' },
+          '50%': { transform: 'translateY(10px) rotate(30deg)' },
+        },
+        lifeHover: {
+          '0%, 100%': { transform: 'translateY(0px) rotate(10deg)' },
+          '50%': { transform: 'translateY(10px) rotate(10deg)' },
+        },
       },
       animation: {
         hoverBarAnim: 'hoverBar 0.3s ease-in 1s forwards',
-        heroWeb: 'popWeb 0.3s cubic-bezier(.39,.87,.91,.48) 0.3s forwards',
+        heroWeb:
+          'popWeb 0.3s cubic-bezier(.39,.87,.91,.48) 0.3s forwards, webHover 2s ease 0.6s infinite',
         heroProgram:
-          'popProgram 0.3s cubic-bezier(.39,.87,.91,.48) 0.6s forwards',
-        heroLife: 'popLife 0.3s cubic-bezier(.39,.87,.91,.48) 0.9s forwards',
-        heroAvatar:
-          'popAvatar 0.3s cubic-bezier(.39,.87,.91,.48) 1s forwards',
+          'popProgram 0.3s cubic-bezier(.39,.87,.91,.48) 0.6s forwards, programHover 2s ease 0.9s infinite',
+        heroLife:
+          'popLife 0.3s cubic-bezier(.39,.87,.91,.48) 0.9s forwards, lifeHover 2s ease 1.2s infinite',
+        heroAvatar: 'popAvatar 0.3s cubic-bezier(.39,.87,.91,.48) 1s forwards',
+        popHover: 'popHover 1s ease infinite',
       },
     },
   },
