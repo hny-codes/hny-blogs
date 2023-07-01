7fe6bc89faff35142895cf65f3a5f029f5eb5097
hny-codes <hnguyhenry@protonmail.com>
2023-07-01 02:59:38 -0700

    Remove netlify

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,13 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
-import image from "@astrojs/image";
-
-import netlify from "@astrojs/netlify/functions";
+import image from '@astrojs/image';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind({
-    applyBaseStyles: false
-  }), image()],
-  output: "server",
-  adapter: netlify()
+  integrations: [
+    tailwind({
+      applyBaseStyles: false,
+    }),
+    image(),
+  ],
 });
