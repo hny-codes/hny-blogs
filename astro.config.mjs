c40cb18b56a7326d8594e1f19d94ec40127dc79e
Henry <hnguyhenry@protonmail.com>
2023-07-01 02:38:01 -0700

    Add netlify adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,14 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
-
 import image from "@astrojs/image";
+
+import netlify from "@astrojs/netlify/functions";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind({
     applyBaseStyles: false
-  }), image()]
+  }), image()],
+  output: "server",
+  adapter: netlify()
 });
