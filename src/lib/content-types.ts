c76fbbc97da85e79ec906c1b054f79ab35615eba
hny-codes <hnguyhenry@protonmail.com>
2023-07-02 20:39:56 -0700

    Add generated types file

---
+++ src/lib/content-types.ts
@@ -0,0 +1,34 @@
+import type {
+  ChainModifiers,
+  Entry,
+  EntryFieldTypes,
+  EntrySkeletonType,
+  LocaleCode,
+} from 'contentful';
+
+export interface TypeBlogPostFields {
+  title: EntryFieldTypes.Symbol;
+  date: EntryFieldTypes.Date;
+  slug: EntryFieldTypes.Symbol;
+  description: EntryFieldTypes.Symbol;
+  content: EntryFieldTypes.RichText;
+}
+
+export type TypeBlogPostSkeleton = EntrySkeletonType<
+  TypeBlogPostFields,
+  'blogPost'
+>;
+
+export type TypeBlogPost<
+  Modifiers extends ChainModifiers,
+  Locales extends LocaleCode
+> = Entry<TypeBlogPostSkeleton, Modifiers, Locales>;
+
+export function isTypeBlogPost<
+  Modifiers extends ChainModifiers,
+  Locales extends LocaleCode
+>(
+  entry: Entry<EntrySkeletonType, Modifiers, Locales>
+): entry is TypeBlogPost<Modifiers, Locales> {
+  return entry.sys.contentType.sys.id === 'blogPost';
+}
