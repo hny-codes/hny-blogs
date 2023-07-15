7171a97ee28f7ea40e7e711dd237d4c13a1edcf5
hny-codes <hnguyhenry@protonmail.com>
2023-07-14 19:23:04 -0700

    Add astro sitemap integration

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,19 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 import image from '@astrojs/image';
+import react from '@astrojs/react';
 
-import react from "@astrojs/react";
+import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind({
-    applyBaseStyles: false
-  }), image(), react()]
+  site: 'https://hny-blogs.vercel.app/',
+  integrations: [
+    tailwind({
+      applyBaseStyles: false,
+    }),
+    image(),
+    react(),
+    sitemap(),
+  ],
 });
