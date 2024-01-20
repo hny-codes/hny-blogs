c3835ce29d991ada191667c9017d30cab420254e
hny-codes <hnguyhenry@protonmail.com>
2024-01-19 18:04:53 -0800

    Change bg color of Share buttons

--- src/components/ShareButton.tsx
+++ src/components/ShareButton.tsx
@@ -26,7 +26,7 @@ export default function ShareButton({ url, social }: Props) {
   return (
     <>
       <button
-        className='btn rounded-3xl border-2 hover:border-[hsl(var(--a))]'
+        className='btn rounded-3xl border-2 bg-[hsl(var(--p))] hover:bg-[hsl(var(--pf))]'
         onClick={handleLink}
       >
         {/* Twitter */}
@@ -39,7 +39,7 @@ export default function ShareButton({ url, social }: Props) {
               viewBox='0 0 24 24'
             >
               <path
-                fill='#888888'
+                fill='#000000'
                 d='M18.205 2.25h3.308l-7.227 8.26l8.502 11.24H16.13l-5.214-6.817L4.95 21.75H1.64l7.73-8.835L1.215 2.25H8.04l4.713 6.231zm-1.161 17.52h1.833L7.045 4.126H5.078z'
               />
             </svg>
@@ -55,7 +55,7 @@ export default function ShareButton({ url, social }: Props) {
             viewBox='0 0 24 24'
           >
             <path
-              fill='#888888'
+              fill='#000000'
               d='M11 17H7q-2.075 0-3.537-1.463T2 12q0-2.075 1.463-3.537T7 7h4v2H7q-1.25 0-2.125.875T4 12q0 1.25.875 2.125T7 15h4zm-3-4v-2h8v2zm5 4v-2h4q1.25 0 2.125-.875T20 12q0-1.25-.875-2.125T17 9h-4V7h4q2.075 0 3.538 1.463T22 12q0 2.075-1.463 3.538T17 17z'
             />
           </svg>
